5690680c2b2972aad2f7ad04c96600ef06f37015
ghduf3 <109141717+ghduf3@users.noreply.github.com>
2022-07-18 19:48:48 +0900

    Remote Git

---
+++ Class/Assets/Animator & BlendTree/script/AnimationAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAction: MonoBehaviour
+{
+    public Animator animator;
+
+    public void Punch()
+    {
+        animator.Play("Punch");
+    }
+
+    public void Running()
+    {
+        animator.Play("Running");
+    }
+
+}
