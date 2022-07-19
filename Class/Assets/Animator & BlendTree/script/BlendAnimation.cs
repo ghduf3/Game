30a1b18ef8c78644699f3db298452634549cb467
ghduf3 <109141717+ghduf3@users.noreply.github.com>
2022-07-19 19:46:08 +0900

    Remote Git

---
+++ Class/Assets/Animator & BlendTree/script/BlendAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+    }
+}
